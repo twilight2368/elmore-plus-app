37de793af285fec55c6bf1811e325fb9a46347bd
ghostoninternet <daosyphuc288@gmail.com>
2024-07-30 23:54:20 +0700

    fix: fix asyncHandler middleware

--- server/src/middlewares/asyncHandler.js
+++ server/src/middlewares/asyncHandler.js
@@ -1,6 +1,6 @@
 function asyncHandler(fn) {
-  return (req, res, next) => {
-    fn(req, res, next).catch(next)
+  return async (req, res, next) => {
+    await fn(req, res, next).catch(next)
   }
 }
 
